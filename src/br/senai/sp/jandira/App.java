02457fbc87bbbb1ae1f4fea441559f5f714410e7
analu-a <analuiza.azevedomorais@gmail.com>
2023-12-14 00:32:54 -0300

    anotação

--- src/br/senai/sp/jandira/App.java
+++ src/br/senai/sp/jandira/App.java
@@ -1,4 +1,8 @@
 package br.senai.sp.jandira;
 
 public class App {
+    /* Vitor esse projeto ficou muito confuso na minha cabeça,
+    * ainda assim tentei fazer, mas não consegui quase nada,
+    * enfim só estou te avisando que eu tentei mas para mim
+    * não foi possivel concluir*/
 }
